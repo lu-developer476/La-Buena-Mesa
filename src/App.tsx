3de4e8be0946984e70556ce1c4fa89e6a1c150db
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-05 01:25:01 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -102,7 +102,7 @@ export default function App() {
 
       <footer>
         <div className="footer-copy">
-          <span className="text-cyber-gold">©</span> {new Date().getFullYear()} Todos los derechos reservados • Built with JavaScript, React.js & TypeScript • UX/UI Interface • Deployed on Vercel ®
+          <span className="text-cyber-gold">©</span> {new Date().getFullYear()} Todos los derechos reservados • Built with React.js & TypeScript • UX/UI Interface • Deployed on Vercel ®
         </div>
       </footer>
     </div>
