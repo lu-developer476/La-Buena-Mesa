a79251109b5e0ccc960afad653d10f20bf40b161
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-05 00:33:56 -0300

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,30 +1,30 @@
 const items = [
-  "bife_parrilla.jpg",
-  "milanesa_napolitana.jpg",
-  "pasta_casera.jpg",
-  "risotto_hongos.jpg",
-  "ensalada_cesar.jpg",
-  "pollo_limon.jpg",
-  "hamburguesa_gourmet.jpg",
-  "tacos_carne.jpg",
-  "pizza_artesanal.jpg",
-  "salmon_grillado.jpg",
-  "vino_tinto.jpg",
-  "vino_blanco.jpg",
-  "cerveza_artesanal.jpg",
+  "bife-parrilla.jpg",
+  "milanesa-napolitana.jpg",
+  "pasta-casera.jpg",
+  "risotto-hongos.jpg",
+  "ensalada-cesar.jpg",
+  "pollo-limon.jpg",
+  "hamburguesa-gourmet.jpg",
+  "tacos-carne.jpg",
+  "pizza-artesanal.jpg",
+  "salmon-grillado.jpg",
+  "vino-tinto.jpg",
+  "vino-blanco.jpg",
+  "cerveza-artesanal.jpg",
   "limonada.jpg",
-  "agua_saborizada.jpg",
-  "flan_caramelo.jpg",
+  "agua-saborizada.jpg",
+  "flan-caramelo.jpg",
   "tiramisu.jpg",
-  "cheesecake_frutos.jpg",
-  "helado_artesanal.jpg",
-  "brownie_chocolate.jpg",
+  "cheesecake-frutos.jpg",
+  "helado-artesanal.jpg",
+  "brownie-chocolate.jpg",
 ]
 
 const prettyName = (fileName: string) =>
   fileName
     .replace(".jpg", "")
-    .split("_")
+    .split("-")
     .join(" ")
     .replace(/\b\w/g, (char) => char.toUpperCase())
 
